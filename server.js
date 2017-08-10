f4fbaf7e54c9a0cb45934abeca51748581532afc
punknight <punknight16@gmail.com>
2017-08-10 15:04:01 -0700

    made a working app without expres

--- server.js
+++ server.js
@@ -1,19 +1,61 @@
 var http = require('http');
+var parse = require('url').parse;
+var entries = [];
 
 var server = http.createServer(function(req, res){
-	res.end('hello world');
+	switch(req.method){
+    case 'POST':
+      if('/createOne'==parse(req.url).pathname){
+        var item = '';
+        req.setEncoding('utf8');
+        req.on('data', function(chunk){
+          item+=chunk;
+        });
+        req.on('end', function(){
+          entries.push(item);
+          show(req, res);  
+        })
+      } else {
+        error(req, res);
+      }
+      break;
+    case 'GET':
+      show(req, res);
+      break;
+    default:
+      error(req, res);
+  }
 }).listen(process.env.PORT || 3000);
 
+function error(req, res){
+  res.end('error');
+}
+function show(req, res){
+  res.end(JSON.stringify(entries));
+}
 
 //Tests
 if (require.main.filename == '/usr/local/lib/node_modules/mocha/bin/_mocha') {
   var assert = require('assert');
   describe('server', function(){
-  	it('should return hello world', function(done){
-  		var req = {};
+  	it('should return entries_arr with entry1', function(done){
+  		var req = { 
+        method: 'POST',
+        url: '/createOne',
+        setEncoding: function(){},
+        on: function(event, cb){
+          if(event=='data'){
+            return cb('entry1');  // this is the post data
+          } else {
+            return cb();
+          }
+        }
+      };
   		var res = { 
-    		end: function(string){ 
-    			assert(string=='hello world');
+    		end: function(entries){ 
+          //console.log(entries);
+          console.log(entries)
+    			assert(entries=='["entry1"]');
     			done();
     		}
     	};
