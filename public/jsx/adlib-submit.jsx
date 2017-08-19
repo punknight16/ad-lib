370026bfeaff0393bfceb4787b44fc72b2ff4360
punknight <punknight16@gmail.com>
2017-08-19 01:23:53 -0700

    added submit component

--- public/jsx/adlib-submit.jsx
+++ public/jsx/adlib-submit.jsx
@@ -1,8 +1,41 @@
-var Bacon = React.createClass({
+var AdlibSubmit = React.createClass({
+  getInitialState: function(){
+    return {
+      person_you_know: "John",
+      noun1: 'fox',
+      noun2: 'dog',
+      adjective: 'lazy'
+    }
+  },
+  getUserInput: function(e){
+    e.preventDefault();    
+    var person_you_know = getData('drag1');
+    var noun1 = getData('drag2');
+    var noun2 = getData('drag3');
+    var adjective = getData('drag4');
+    this.setState({
+     person_you_know: person_you_know,
+      noun1: noun1,
+      noun2: noun2,
+      adjective: adjective 
+    }, ()=>{
+      this.submit_ghost.submit();
+    });
+  },
   render: function (){
     return (
-      <div>Cool Button</div>
+      <div>
+        <form action='/createOne' method='POST' ref={(ref) => this.submit_ghost = ref}>
+          <input type="hidden" value={this.state.person_you_know} name="person_you_know" />
+          <input type="hidden" value={this.state.noun1} name="noun1" />
+          <input type="hidden" value={this.state.noun2} name="noun2" />
+          <input type="hidden" value={this.state.adjective} name="adjective" />
+        </form>
+        <form onSubmit={this.getUserInput}>
+          <input type='submit' className='submit-button' value='Submit' />
+        </form>
+      </div>
     );
   }
 });
-ReactDOM.render(<Bacon />,document.getElementById('submit'));
+ReactDOM.render(<AdlibSubmit />,document.getElementById('submit'));
